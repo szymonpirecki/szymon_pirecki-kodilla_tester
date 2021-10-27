7b7dbb8c51f038505dd7c84845684fba58051911
Szymon Pirecki <szymon.pirecki@gmail.com>
2021-10-27 17:59:48 +0200

    Main and Main Extended

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Szymon";
+        int myAge = 24;
+        double height = 1.93;
+        char carClass = 'B';
+        boolean isGoodPerson = true;
+        String start = "Hello from first class";
+        System.out.println(start);
+        System.out.println(myName);
+        System.out.println(carClass);
+        System.out.println(myAge);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args){
+        String bestMovie = "Cath me if you can";
+        int yearOfRelease = 2003;
+        boolean trueOrFalse = true;
+        char firstLetterOfTheNameOfTheMainActor = 'L';
+        int minutes = 141;
+        String director = "Spielberg";
+        System.out.println(bestMovie);
+        System.out.println(yearOfRelease);
+        System.out.println(trueOrFalse);
+        System.out.println(firstLetterOfTheNameOfTheMainActor);
+        System.out.println(minutes);
+        System.out.println(director);
+
+
+    }
+}
