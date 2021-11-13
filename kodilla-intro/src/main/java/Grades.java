0b4070588ddbc309ec9009398fe02beba947f759
Szymon Pirecki <szymon.pirecki@gmail.com>
2021-11-13 12:54:32 +0100

    poprawione grades z komentarzem + prośba o odwołanie do komentarzy

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -1,12 +1,12 @@
 public class Grades {           //utworzenie klasy Grades
-    private int[] grades;       //utworzenie tablicy czy przypisanie jej do klasy?
-    private int size;           //size - nowa zmienna do monitorowania ile jest elementow w tablicy. przypisanie nowej zmiennej do klasy
-    private int value;          //przypisanie zmiennej value do klasy
+    private int[] grades;       //dodanie atrybutu klasy - tablicy grades
+    private int size;           //dodanie do klasy atrybutu "size" - nowa zmienna do monitorowania ile jest elementow w tablicy.
+    private int value;          //dodanie do klasy atrybutu "value"
 
-    public Grades() {           //utworzenie obiektu Grades, ktory ponizej okreslamy ze jest tablica
-        this.grades = new int[10];  //"przypisanie nowej wartosci do zmiennej przechowujacej oceny" - czyli ustanowienie ze grades jest tablica i ma 10 elementów?
+    public Grades() {           //utworzenie konstruktora grades
+        this.grades = new int[10];  //ustanowienie ze tablica grades ma 10 elementów?
         this.size = 0;          // zadeklarowanie ze zmienna size = 0
-        this.value = value;     //przypisanie argumentu do zmiennej
+        this.value = value;     //przypisanie argumentu value do atrybutu klasy
     }
 
     public void add(int value) {  //utworzenie metody add zeby dodawac oceny do tablicy. (int value) - metoda przyjmuje parametr value
@@ -14,24 +14,29 @@ public class Grades {           //utworzenie klasy Grades
             return;
         }
         this.grades[this.size] = value; //instrukcja, zeby dopoki warunek if jest spelniany lokowac nowe oceny(value) na nowych miejscach w tablicy
-        this.size++;
+        this.size++;    //instrukcja, zeby zwiekszac wartosc size z kazdym nastepnym dodaniem oceny
     }
 
-    public int lastGrade() {    //utworzenie metody lastGrade
-        if (this.value > 0) { //warunek, ze metoda dziala jesli ocena jest wieksza od zera, chcialem to zrobic od strony indeksow ale one sa numerowane od 0
-            int lastPlace = grades.length - 1; //okreslenie ostatniego indeksu - nie wiedzialem jak uzyc do tego zmiennej "size"
-            int theLastGrade = grades[lastPlace]; // zadeklarowanie zmiennej theLastGrade, ze sie rowna ostatniej ocenie
-            return theLastGrade;    //zwrocenie ostatniej oceny - nie wiem czemu pojawia sie problem "missing return statement"
+    public int lastGrade() {                           //utworzenie metody lastGrade
+        if (grades.length == 0) {
+            System.out.println("There isnt any grade");
+        } else {
+            int lastPlace = grades.length - 1;          //okreslenie ostatniego indeksu
+            return grades[lastPlace];                         //zwrocenie ostatniej oceny - nie wiem czemu pojawia sie problem "missing return statement"
         }
     }
 
 
     public int avarage() { //utworzenie metody avarage
-        int result = 0;
-        for (int i = 0; i < grades.length; i++) {
-            result = result + grades[i];    //utworzenie petli for zeby sumowac elementy tablicy
+        if (grades.length == 0) {
+            System.out.println("There isnt any grade");
+        } else {
+            int result = 0;
+            for (int i = 0; i < grades.length; i++) {
+                result = result + grades[i];    //utworzenie petli for zeby sumowac elementy tablicy
+            }
+            return result / grades.length;  //zwrot średniej
         }
-        return result / grades.length;  //zwrot średniej
     }
 }
 //cześć Paweł, mam prosbe.
