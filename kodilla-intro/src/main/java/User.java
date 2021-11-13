772b2ef4e830ff40f87a43b2a2cbd0c333016d62
Szymon Pirecki <szymon.pirecki@gmail.com>
2021-11-13 22:46:28 +0100

    User - dlaczego pojawia mi sie blad przy warunku if w metodzie showWhoIsBelowAverageAge?

--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -1,19 +1,50 @@
 public class User {
-   String name;
-   int age;
-
+    String name;
+    int age;
+    User users[];
 
 
     public User(String name, int age) {
         this.name = name;
         this.age = age;
+        this.users = new User[8];
+    }
+
+    public void average() {
+
+        int result = 0;
+        for (int i = 0; i < users.length; i++){
+            result = result + users[i].age;
+        }
+        int theAverage = result / users.length;
+    }
+
+    public void showWhoIsBelowAverageAge() {
+
+        for (int i = 0; i < users.length; i++){
+            if(this.age < int theAverage){         //dlaczego tutaj warunek if nie chce mi przejsc?
+                System.out.println(this.name);
+            }
+        }
     }
 
 
 
         public static void main(String[] args){
-User[] Users;
-            Users = new User[];
+
+        User Kamil = new User("Kamil", 16);
+        User Zbyszek = new User("Zbyszek", 24);
+        User Adam = new User("Adam", 39);
+        User Tomasz = new User("Tomasz", 8);
+        User Olaf = new User("Olaf", 88);
+        User Gracjan = new User("Gracjan", 18);
+        User Krzysztof = new User("Krzysztof", 54);
+        User Przemek = new User("Przemek", 33);
+
+        User users[] = {Kamil, Zbyszek, Adam, Tomasz, Olaf, Gracjan, Krzysztof, Przemek};
+
+
+
 
         }
 
