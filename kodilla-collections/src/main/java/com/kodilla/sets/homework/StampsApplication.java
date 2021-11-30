e5e81484984a94dc6ad6aa6625dd36dcd2e57b84
Szymon Pirecki <szymon.pirecki@gmail.com>
2021-11-30 11:13:02 +0100

    zadanie 4.4

---
+++ kodilla-collections/src/main/java/com/kodilla/sets/homework/StampsApplication.java
@@ -0,0 +1,29 @@
+package com.kodilla.sets.homework;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StampsApplication {
+    public static void main(String[] args) {
+        Set<Stamp> stamps = new HashSet<>();
+        stamps.add(new Stamp("Donald duck", "1.5x1.5", false));
+        stamps.add(new Stamp("Mickey Mouse", "1.5x1.2", true));
+        stamps.add(new Stamp("Mickey Mouse", "1.5x1.2", false));
+        stamps.add(new Stamp("Mickey Mouse", "1.5x1.2", true));
+        stamps.add(new Stamp("Nirvana", "1x1", true));
+
+        System.out.println("W tej kolekcji są: " + stamps.size() + " znaczki:");
+        for(Stamp stamp : stamps){
+            System.out.println(stamp);
+        }
+        System.out.println("---------------------------------------");
+        stamps.remove(new Stamp("Nirvana", "1x1", true));
+        System.out.println("usunięto jeden znaczek");
+        System.out.println("---------------------------------------");
+        System.out.println("W tej kolekcji są: " + stamps.size() + " znaczki:");
+        for(Stamp stamp : stamps){
+            System.out.println(stamp);
+        }
+
+    }
+}
