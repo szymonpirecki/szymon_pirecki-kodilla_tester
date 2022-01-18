061df793aa7ee3fdfc06d22f4a82bf11d81a3ed5
Szymon Pirecki <szymon.pirecki@gmail.com>
2022-01-18 16:57:57 +0100

    zadanie gambling machine

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -1,5 +1,28 @@
 package com.kodilla.parametrized_tests.homework;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
 class GamblingMachineTestSuite {
+
+    GamblingMachine gamblingMachine = new GamblingMachine();
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/userNumbers.csv")
+    public void shouldThrowAnException(String input){
+        Set<Integer> userNumbers = new HashSet<>();
+        String numbers[] = input.split(",");
+        for (String number : numbers) {
+            Integer numberAsInteger = Integer.parseInt(number);
+            userNumbers.add(numberAsInteger);
+        }
+        Assertions.assertThrows(InvalidNumbersException.class, () ->gamblingMachine.howManyWins(userNumbers));
+
+
+    }
 
 }
